7cc24a59711911164da1fd33ab88e2a6728ff331
G984822019 <90735672+G984822019@users.noreply.github.com>
2024-08-01 18:40:27 +0900

    update

--- List/readJson.py
+++ List/readJson.py
@@ -5,12 +5,13 @@ with open(sys.argv[1]) as f: # コマンド引数のファイルを開く
         dic = json.loads(line) # 一行のJSONデータを辞書形式に変換する
         if dic['eventid'] == 'cowrie.command.input':
 
-        #ip = dic['src_ip']
-        #print('[src_ip] =', dic['src_ip'])
-        #print(dic['input'])
+       #if dic['src_ip'] != ip:
+        #       ip = dic['src_ip']
+        #       print('[src_ip] =', dic['src_ip'])
+       #print(dic['input'])
         
-            if cmd in dic['input']:
+           if cmd in dic['input']: 
                 if dic['src_ip'] != ip:
-                  ip = dic['src_ip']
-                  print('[src_ip] =', dic['src_ip'])
-                  print(dic['input'])
+                   ip = dic['src_ip']
+                   print('[src_ip] =', dic['src_ip'])
+                print(dic['input'])
